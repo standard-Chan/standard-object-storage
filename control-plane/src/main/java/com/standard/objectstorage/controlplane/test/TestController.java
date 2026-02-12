2aa8f7cc5f186d27bd8631b330a14c1d5ac54a42
standard-chan <starp321@naver.com>
2026-02-12 13:14:44 +0900

    feat: health check test API 추가

---
+++ control-plane/src/main/java/com/standard/objectstorage/controlplane/test/TestController.java
@@ -0,0 +1,14 @@
+package com.standard.objectstorage.controlplane.test;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "okay";
+    }
+
+
