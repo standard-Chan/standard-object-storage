b2a7ed7f5f6b63fb825120cd07e9d2c64dc8e91e
standard-chan <starp321@naver.com>
2026-02-12 12:16:14 +0900

    spring boot init

---
+++ control-plane/src/test/java/com/standard/objectstorage/controlplane/ControlPlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.standard.objectstorage.controlplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlPlaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
