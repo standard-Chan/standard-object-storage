bf075829db97533b771fd01af63b26c530008104
standard-chan <starp321@naver.com>
2026-02-19 16:18:20 +0900

    feat: app.ts 환경변수 세팅

--- storage-node/src/app.ts
+++ storage-node/src/app.ts
@@ -1,39 +1,33 @@
-import { join } from 'node:path'
-import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload'
-import { FastifyPluginAsync, FastifyServerOptions } from 'fastify'
+import dotenv from "dotenv";
+import { join } from "node:path";
+import AutoLoad, { AutoloadPluginOptions } from "@fastify/autoload";
+import { FastifyPluginAsync, FastifyServerOptions } from "fastify";
 
-export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
+dotenv.config();
+export interface AppOptions
+  extends FastifyServerOptions, Partial<AutoloadPluginOptions> {}
+// CLI 명령어를 통해 --options 인자로 추가 옵션 전달하는 용도
+const options: AppOptions = {};
 
-}
-// Pass --options via CLI arguments in command to enable these options.
-const options: AppOptions = {
-}
-
+/**
+ *  Fastify 앱의 메인 플러그인 함수
+ * @param fastify Fastify 인스턴스 (서버 객체)
+ * @param opts 전달받은 옵션들
+ */
 const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
-  opts
+  opts,
 ): Promise<void> => {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  // eslint-disable-next-line no-void
   void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-    options: opts
-  })
+    dir: join(__dirname, "plugins"),
+    options: opts,
+  });
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  // eslint-disable-next-line no-void
   void fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-    options: opts
-  })
-}
+    dir: join(__dirname, "routes"),
+    options: opts,
+  });
+};
 
-export default app
-export { app, options }
+export default app;
+export { app, options };
