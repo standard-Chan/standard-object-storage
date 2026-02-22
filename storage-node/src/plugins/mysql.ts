0f04350ee286327109fb6d5d886bf859f3f51e9e
standard-chan <starp321@naver.com>
2026-02-22 14:52:07 +0900

    chore: storage-node 의 connection pool 설정 변경

--- storage-node/src/plugins/mysql.ts
+++ storage-node/src/plugins/mysql.ts
@@ -1,5 +1,5 @@
-import fp from 'fastify-plugin'
-import fastifyMysql, { FastifyMySQLOptions } from '@fastify/mysql'
+import fp from "fastify-plugin";
+import fastifyMysql, { FastifyMySQLOptions } from "@fastify/mysql";
 
 /**
  * MySQL 데이터베이스 연결 플러그인
@@ -12,39 +12,44 @@ export default fp<MySQLPluginOptions>(async (fastify) => {
   const mysqlOptions: FastifyMySQLOptions = {
     promise: true,
     connectionString: buildMySQLConnectionString(),
-  }
 
-  fastify.register(fastifyMysql, mysqlOptions)
+    connectionLimit: 20,
+    waitForConnections: true,
+    queueLimit: 0,
+  };
+
+  fastify.register(fastifyMysql, mysqlOptions);
 
   // 서버 시작 시 연결 테스트
-  fastify.addHook('onReady', async function () {
+  fastify.addHook("onReady", async function () {
     try {
-      const connection = await fastify.mysql.getConnection()
-      fastify.log.info('MySQL 데이터베이스에 성공적으로 연결되었습니다')
-      connection.release()
+      const connection = await fastify.mysql.getConnection();
+      fastify.log.info("MySQL 데이터베이스에 성공적으로 연결되었습니다");
+      connection.release();
     } catch (err) {
-      fastify.log.error({ err }, 'MySQL 연결 실패')
-      throw err
+      fastify.log.error({ err }, "MySQL 연결 실패");
+      throw err;
     }
-  })
+  });
 
   // 서버 종료 시 연결 정리
-  fastify.addHook('onClose', async (instance) => {
-    await instance.mysql.pool.end()
-    instance.log.info('MySQL 연결이 종료되었습니다')
-  })
-})
+  fastify.addHook("onClose", async (instance) => {
+    await instance.mysql.pool.end();
+    instance.log.info("MySQL 연결이 종료되었습니다");
+  });
+});
 
+/** 연결 설정 string */
 function buildMySQLConnectionString(): string {
   if (process.env.MYSQL_CONNECTION_STRING) {
-    return process.env.MYSQL_CONNECTION_STRING
+    return process.env.MYSQL_CONNECTION_STRING;
   }
 
-  const user = process.env.MYSQL_USER || 'root'
-  const password = process.env.MYSQL_PASSWORD || 'password'
-  const host = process.env.MYSQL_HOST || 'localhost'
-  const port = process.env.MYSQL_PORT || '3306'
-  const database = process.env.MYSQL_DATABASE || 'test'
+  const user = process.env.MYSQL_USER || "root";
+  const password = process.env.MYSQL_PASSWORD || "password";
+  const host = process.env.MYSQL_HOST || "localhost";
+  const port = process.env.MYSQL_PORT || "3306";
+  const database = process.env.MYSQL_DATABASE || "test";
 
-  return `mysql://${user}:${password}@${host}:${port}/${database}`
+  return `mysql://${user}:${password}@${host}:${port}/${database}`;
 }
